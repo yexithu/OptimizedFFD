69df6af8edc1619c1181a4075cc965b88f73f298
Zhicheng Gu <jimmygoo@synapse.ices.utexas.edu>
2017-07-08 11:48:03 -0500

    CalcTransCP.m

--- CalcTransCP.m
+++ CalcTransCP.m
@@ -4,7 +4,6 @@ function [rotCell] = CalcTransCP(curCP, dstCP, scale)
     end
 
     %%first divide control points
-    groupSize = 3 * 3 * 3;
 %     stride = 1;
     l = 8;
     m = 8;
@@ -15,57 +14,32 @@ function [rotCell] = CalcTransCP(curCP, dstCP, scale)
     % for a single cell
     for i = 1:(l+1)
 
-        if i == 1
-            startI = i;
-        else
-            startI = i-1;
-        end
-        if i == l+1
-            endI = i;
-        else
-            endI = i+1;
-        end
+        [startI, endI, lengthI, centerI] = getNbrArg(i, l+1);
         
         for j = 1:(m+1)
-            if j == 1
-                startJ = j;
-            else
-                startJ = j-1;
-            end
-            
-            if j == m+1
-                endJ = j;
-            else
-                endJ = j+1;
-            end
+           
+            [startJ, endJ, lengthJ, centerJ] = getNbrArg(j, m+1);
             
             for k = 1:(n+1)
-                if k == 1
-                    startK = 1;
-                else
-                    startK = k+1;
-                end
                 
-                if k == n+1
-                    endK = k;
-                else
-                    endK = k+1;
-                end
-                    
+                [startK, endK, lengthK, centerK] = getNbrArg(k, n+1);
                 
                 curCube = curCP(startI:endI,startJ:endJ,startK:endK);
                 dstCube = dstCP(startI:endI,startJ:endJ,startK:endK);
                 
-                curCenter = curCube(2,2,2);
-                dstCenter = dstCube(2,2,2);
+                curCenter = curCube(centerI,centerJ,centerK);
+                dstCenter = dstCube(centerI,centerJ,centerK);
+                
+                groupSize = lengthI * lengthJ * lengthK;
 
                 curEdgeWithCenter = curCenter - reshape(cell2mat(curCube), 3, groupSize);
                 dstEdgeWithCenter = dstCenter - reshape(cell2mat(dstCube), 3, groupSize);
 
                 curEdge = curEdgeWithCenter;
-                curEdge(:, (groupSize + 1)/2) = [];
+                centerIdx = find(curEdge==[0,0,0]');
+                curEdge(:, centerIdx) = [];
                 dstEdge = dstEdgeWithCenter;
-                dstEdge(:, (groupSize + 1)/2) = [];
+                dstEdge(:, centerIdx) = [];
 
 
                 [idx, dist] = knnsearch(curEdge', dstEdge');
@@ -85,4 +59,25 @@ function [rotCell] = CalcTransCP(curCP, dstCP, scale)
         end
     end
     
+end
+
+function [startX, endX, centerX, lengthX] = getNbrArg(x, maxX)
+    if x == 1
+        startX = 1;
+        centerX = 1;
+    else
+        startX = x-1;
+        centerX = 2;
+    end
+
+    if x == maxX
+        endX = x;
+    else
+        endX = x+1;
+    end
+
+    lengthX = 3;
+    if x == 1 || x == maxX
+        lengthX = 2;
+    end
 end
