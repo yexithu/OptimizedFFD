00773933420b073662fbc546215deafbb7d1fa11
JimmyGoo <guzc14@mails.tsinghua.edu.cn>
2017-07-21 14:36:11 -0500

    fix bug in Evaluate.m

--- Evaluate.m
+++ Evaluate.m
@@ -1,9 +1,5 @@
-function [pPoints, qPoints, err, meanErr] = Evaluate(pPath, qPath, pArray, qArray)
-    pIdx = importdata(pPath);
-    qIdx = importdata(qPath);
-    pPoints = pArray(:, pIdx);
-    qPoints = qArray(:, qIdx);
-    err = pPoints - pPoints;
+function [err, meanErr] = EvaluateEuclid(pPoints, qPoints)
+    err = pPoints - qPoints;
     err = err.^2;
     err = sum(err, 1);
     meanErr = mean(err);
