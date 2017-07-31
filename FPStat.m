8b618d33b52e1b67aff8167f5a99f0ebbd6db241
yexithu <yexithu14@163.com>
2017-07-31 17:35:53 -0500

    For data clean

--- FPStat.m
+++ FPStat.m
@@ -1,28 +1,60 @@
-dataRoot = '/h2/yexi/Desktop/data/healthy/';
-resultRoot = '/h2/yexi/Desktop/result/';
 
-availableIdx = [1:1:94];
-availableIdx(availableIdx == 1) = [];
-availableIdx(availableIdx == 10) = [];
-availableIdx(availableIdx == 74) = [];
+global config;
+config = GenConfig();
 
-m = length(availableIdx);
+modelIDs = config.modelIDs;
+m = length(modelIDs);
 
-dist = zeros(m, 37);
+healthyRoot = config.healthyRoot;
+resultRoot = config.resultRoot;
 
-for i = 1:m
+% dist = zeros(m, 37);
 
-    idx = availableIdx(i);
-    model = load([dataRoot 'mat/' num2str(idx) '.mat']);
+% for i = 1:m
 
-    lmIdx1 = importdata([dataRoot 'landmark1/' num2str(idx) '.txt']);
-    lmIdx2 = importdata([dataRoot 'landmark2/' num2str(idx) '.txt']);
+%     idx = modelIDs(i);
+%     model = load([healthyRoot 'mat/' num2str(idx) '.mat']);
 
-    pts1= model.p(:, lmIdx1);
-    pts2= model.p(:, lmIdx2);
+%     lmIdx1 = importdata([healthyRoot 'landmark1/' num2str(idx) '.txt']);
+%     lmIdx2 = importdata([healthyRoot 'landmark2/' num2str(idx) '.txt']);
 
-    dist(i, :) = dot(pts1 - pts2, pts1 - pts2);
+%     pts1= model.p(:, lmIdx1);
+%     pts2= model.p(:, lmIdx2);
+
+%     dist(i, :) = dot(pts1 - pts2, pts1 - pts2);
+% end
+% save('dist.mat', 'dist');
+dist = load('dist.mat');
+dist = dist.dist;
+
+goodIdx = [2, 3, 4, 5, 6, 7, 8, 14, 15, 17, 18, 19, 20, 21, 22, 23, 28, 29, 31, 32, 33, 34, 35, 36, 37];
+mu = mean(dist, 1);
+sigma = std(dist);
+upper = mu + 3 * sigma;
+toVerify = dist > ones(91, 1) * upper;
+toVerify = toVerify(:, goodIdx);
+for i = 1: length(modelIDs)
+    if sum(toVerify(i, :) > 0)
+        fprintf('%d\n', modelIDs(i));
+
+        idx = modelIDs(i);
+        model = load([healthyRoot 'mat/' num2str(idx) '.mat']);
+
+        lmIdx1 = importdata([healthyRoot 'landmark1/' num2str(idx) '.txt']);
+        lmIdx2 = importdata([healthyRoot 'landmark2/' num2str(idx) '.txt']);
+
+        pts1= model.p(:, lmIdx1);
+        pts2= model.p(:, lmIdx2);
+
+        for j = 1:length(goodIdx)
+            if (toVerify(i, j) > 0)
+                fprintf('\t%d\n', goodIdx(j));
+                pts1(:, goodIdx(j))
+                pts2(:, goodIdx(j))
+            end
+        end
+        fprintf('\n');
+        pause;
+    end
 end
-
-save([resultRoot 'dist.mat'], 'dist');
 
